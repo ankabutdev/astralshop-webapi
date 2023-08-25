59737ffc2304aa587c7fc4642227605744b9c166
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-08-25 16:29:00 +0500

    IUnitOfWork Added

---
+++ src/AstralShop.DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using AstralShop.Domain.Entities.Categories;
+using AstralShop.Domain.Entities.Companies;
+using AstralShop.Domain.Entities.Deliveries;
+using AstralShop.Domain.Entities.Discounts;
+using AstralShop.Domain.Entities.Orders;
+using AstralShop.Domain.Entities.Products;
+using AstralShop.Domain.Entities.Users;
+
+namespace AstralShop.DataAccess.Interfaces;
+
+public interface IUnitOfWork
+{
+    public IRepository<Category> CategoryRepository { get; }
+
+    public IRepository<Company> CompanyRepository { get; }
+
+    public IRepository<Deliver> DeliverRepository { get; }
+
+    public IRepository<Discount> DiscountRepository { get; }
+
+    public IRepository<Order> OrderRepository { get; }
+
+    public IRepository<Product> ProductRepository { get; }
+
+    public IRepository<User> UserRepository { get; }
+
+    public Task<bool> SaveAsync();
+}
