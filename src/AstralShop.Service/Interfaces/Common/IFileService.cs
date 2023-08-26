827a765a67bda624cc18cda3fa47ac05678baf07
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-08-26 20:24:38 +0500

    Add IFileService in Service

---
+++ src/AstralShop.Service/Interfaces/Common/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AstralShop.Service.Interfaces.Common;
+
+public interface IFileService
+{
+    public Task<string> UploadImageAsync(IFormFile image);
+
+    public Task<bool> DeleteImageAsync(string subpath);
+
+    public Task<string> UploadAvatarAsync(IFormFile avatar);
+
+    public Task<bool> DeleteAvatarAsync(string subpath);
+}
