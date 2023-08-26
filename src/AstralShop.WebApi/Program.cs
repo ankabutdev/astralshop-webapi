ca3e146b47c2f479c65e4e5138ab17bd264d2b8d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-08-27 04:20:41 +0500

    Improved

--- src/AstralShop.WebApi/Program.cs
+++ src/AstralShop.WebApi/Program.cs
@@ -19,7 +19,7 @@ var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-builder.Configuration["Serilog:WriteTo:0:Args:path"] = AppSettingHelper.GetLogFilePath();
+//builder.Configuration["Serilog:WriteTo:0:Args:path"] = AppSettingHelper.GetLogFilePath();
 
 //var logger = new LoggerConfiguration()
 //        .ReadFrom.Configuration(builder.Configuration)
