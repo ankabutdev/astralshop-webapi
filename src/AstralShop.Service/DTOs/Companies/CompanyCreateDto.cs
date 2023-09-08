dd327af190b529cace831fd9a3f92179d2dfdb17
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-09-08 16:07:19 +0500

    Added CompanyDTOs in DTOs

---
+++ src/AstralShop.Service/DTOs/Companies/CompanyCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace AstralShop.Service.DTOs.Companies;
+
+public class CompanyCreateDto
+{
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    [MaxLength(13)]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public IFormFile ImagePath { get; set; } = default!;
+}
---
+++ src/AstralShop.Service/DTOs/Companies/CompanyUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace AstralShop.Service.DTOs.Companies;
+
+public class CompanyUpdateDto
+{
+    public long Id { get; set; }
+
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    [MaxLength(13)]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public IFormFile? ImagePath { get; set; }
+}
