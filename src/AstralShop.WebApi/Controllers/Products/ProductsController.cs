3b988aab92e7e99e71e3fe25a4e82317578eb1c4
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-09-08 11:21:11 +0500

    Added ProductController

--- src/AstralShop.WebApi/Controllers/Products/ProductsController.cs
+++ src/AstralShop.WebApi/Controllers/Products/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AstralShop.DataAccess.Utils;
+using AstralShop.Service.DTOs.Products;
+using AstralShop.Service.Interfaces.Products;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AstralShop.WebApi.Controllers.Products;
 
@@ -6,5 +9,19 @@ namespace AstralShop.WebApi.Controllers.Products;
 [ApiController]
 public class ProductsController : ControllerBase
 {
+    private readonly IProductService _service;
+    private readonly int _maxPageSize = 30;
 
+    public ProductsController(IProductService service)
+    {
+        this._service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] int page = 1)
+        => Ok(await _service.GetAllAsync(new PaginationParams(page, _maxPageSize)));
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(ProductCreateDto dto)
+        => Ok(await _service.CreateAsync(dto));
 }
