b4308315142ee05be6a7c11fa04a3fcd33c6a742
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-09-08 11:24:04 +0500

    Added ProductService body

---
+++ src/AstralShop.Service/Services/Products/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using AstralShop.DataAccess.Utils;
+using AstralShop.Service.DTOs.Products;
+using AstralShop.Service.Interfaces.Products;
+
+namespace AstralShop.Service.Services.Products;
+
+public class ProductService : IProductService
+{
+    public Task<long> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductRusultDto> CreateAsync(ProductCreateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ProductRusultDto>> GetAllAsync(PaginationParams @params)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductRusultDto> GetByIdAsync(long productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductRusultDto> UpdateAsync(ProductUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateImageAsync(long productId, string imagePath)
+    {
+        throw new NotImplementedException();
+    }
+}
