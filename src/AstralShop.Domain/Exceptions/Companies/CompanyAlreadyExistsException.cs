b56c01659a02509488296f65291fc462bc9ccf68
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-09-09 17:21:44 +0500

    Added CompanyAlreadyExistsException in Exceptions

---
+++ src/AstralShop.Domain/Exceptions/Companies/CompanyAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace AstralShop.Domain.Exceptions.Companies;
+
+public class CompanyAlreadyExistsException : NotFoundException
+{
+    public CompanyAlreadyExistsException()
+    {
+        this.TitleMessage = "Company already exists!";
+    }
+}
