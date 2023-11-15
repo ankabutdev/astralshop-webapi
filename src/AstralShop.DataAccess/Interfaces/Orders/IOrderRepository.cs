df9b4e849a1419fe8e94cf1fa8fe9b44595899aa
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-15 23:06:59 +0500

    add Orders interface

---
+++ src/AstralShop.DataAccess/Interfaces/Orders/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using AstralShop.DataAccess.Common.Interface;
+using AstralShop.Domain.Entities.Orders;
+
+namespace AstralShop.DataAccess.Interfaces.Orders;
+
+public interface IOrderRepository : IRepository<Order>,
+    IGetAll<Order>, ISearchable<Order>
+{
+}
